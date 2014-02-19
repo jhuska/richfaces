fa9758c1fa7cc3d1d6d48e58e708b5e23c28296f
Michal Petrov <mpetrov@redhat.com>
2014-02-19 17:24:49 +0100

    submit files via XHR2

--- framework/src/main/resources/META-INF/resources/org.richfaces/input/fileUpload/fileupload.js
+++ framework/src/main/resources/META-INF/resources/org.richfaces/input/fileUpload/fileupload.js
@@ -45,10 +45,14 @@
         this.inputContainer = this.addButton.find(".rf-fu-inp-cntr:first");
         this.input = this.inputContainer.children("input");
         this.list = header.next();
+        this.list.bind('dragenter', function(e) {e.stopPropagation(); e.preventDefault();});
+        this.list.bind('dragover', function(e) {e.stopPropagation(); e.preventDefault();});
+        this.list.bind('drop', $.proxy(this.__addItemsFromDrop, this));
+
         this.hiddenContainer = this.list.next();
-        this.iframe = this.hiddenContainer.children("iframe:first");
-        this.progressBarElement = this.iframe.next();
-        this.progressBar = rf.component(this.progressBarElement);
+        //this.iframe = this.hiddenContainer.children("iframe:first");
+        //this.progressBarElement = this.iframe.next();
+        //this.progressBar = rf.component(this.progressBarElement);
         this.cleanInput = this.input.clone();
         this.addProxy = $.proxy(this.__addItems, this);
         this.input.change(this.addProxy);
@@ -57,7 +61,7 @@
             .mouseup(unpressButton).mouseout(unpressButton);
         this.clearButton.click($.proxy(this.__removeAllItems, this)).mousedown(pressButton)
             .mouseup(unpressButton).mouseout(unpressButton);
-        this.iframe.load($.proxy(this.__load, this));
+        //this.iframe.load($.proxy(this.__load, this));
         if (this.onfilesubmit) {
             rf.Event.bind(this.element, "onfilesubmit", new Function("event", this.onfilesubmit));
         }
@@ -100,7 +104,7 @@
     };
 
     rf.BaseComponent.extend(rf.ui.FileUpload);
-    
+
 
     function TypeRejectedException(fileName) {
         this.name = "TypeRejectedException";
@@ -129,7 +133,7 @@
                 var multipleInputFiles = this.input.prop("files");
                 if (multipleInputFiles) {
                     for (var i = 0 ; i < multipleInputFiles.length; i++) {
-                        this.__tryAddItem(context, multipleInputFiles[i].name);
+                        this.__tryAddItem(context, multipleInputFiles[i]);
 
                         if (this.maxFilesQuantity && (context.acceptedFileNames.length >= this.maxFilesQuantity)) {
                             this.addButton.hide();
@@ -150,22 +154,51 @@
                 }
             },
 
-            __tryAddItem: function(context, fileName) {
+            __addItemsFromDrop: function(dropEvent) {
+                dropEvent.stopPropagation();
+                dropEvent.preventDefault();
+
+                var context = {
+                    acceptedFileNames: [],
+                    rejectedFileNames: []
+                };
+
+                var dropFiles = dropEvent.originalEvent.dataTransfer.files;
+
+                    for (var i = 0 ; i < dropFiles.length; i++) {
+                        this.__tryAddItem(context, dropFiles[i]);
+
+                        if (this.maxFilesQuantity && (context.acceptedFileNames.length >= this.maxFilesQuantity)) {
+                            this.addButton.hide();
+                            break;
+                        }
+                    }
+
+                if (context.rejectedFileNames.length > 0) {
+                    rf.Event.fire(this.element, "ontyperejected", context.rejectedFileNames.join(','));
+                }
+
+                if (this.immediateUpload) {
+                    this.__startUpload();
+                }
+            },
+
+            __tryAddItem: function(context, file) {
                 try {
-                    if (this.__addItem(fileName)) {
-                        context.acceptedFileNames.push(fileName);
+                    if (this.__addItem(file)) {
+                        context.acceptedFileNames.push(file.name);
                     }
                 } catch (e) {
                     if (e instanceof TypeRejectedException) {
-                        context.rejectedFileNames.push(fileName);
+                        context.rejectedFileNames.push(file.name);
                     } else {
                         throw e;
                     }
                 }
             },
 
-            __addItem: function(fileName) {
-                var fileName = fileName;
+            __addItem: function(file) {
+                var fileName = file.name;
                 if (!navigator.platform.indexOf("Win")) {
                     fileName = fileName.match(/[^\\]*$/)[0];
                 } else {
@@ -178,7 +211,7 @@
                 if (this.__accept(fileName) && (!this.noDuplicate || !this.__isFileAlreadyAdded(fileName))) {
                     this.input.hide();
                     this.input.unbind("change", this.addProxy);
-                    var item = new Item(this, fileName);
+                    var item = new Item(this, file);
                     this.list.append(item.getJQuery());
                     this.items.push(item);
                     this.input = this.cleanInput.clone();
@@ -234,73 +267,75 @@
             },
 
             __startUpload: function() {
+                if (!this.items.length) {return;}
                 this.loadableItem = this.items.shift();
                 this.__updateButtons();
                 this.loadableItem.startUploading();
             },
 
-            __submit: function() {
-                var encodedURLInputs = this.form.children("input[name='javax.faces.encodedURL']");
-                var originalAction = encodedURLInputs.length > 0 ? encodedURLInputs.val() : this.form.attr("action");
-                var originalEncoding = this.form.attr("encoding");
-                var originalEnctype = this.form.attr("enctype");
-                try {
-                    var delimiter = originalAction.indexOf("?") == -1 ? "?" : "&";
-                    this.form.attr("action", originalAction + delimiter + UID + "=" + this.loadableItem.uid);
-                    this.form.attr("encoding", "multipart/form-data");
-                    this.form.attr("enctype", "multipart/form-data");
-                    rf.submitForm(this.form, {"org.richfaces.ajax.component": this.id}, this.id);
-                    rf.Event.fire(this.element, "onfilesubmit", this.loadableItem.model);
-                } finally {
-                    this.form.attr("action", originalAction);
-                    this.form.attr("encoding", originalEncoding);
-                    this.form.attr("enctype", originalEnctype);
-                    this.loadableItem.input.removeAttr("name");
-                }
-            },
+             __submit: function() {
+                // var encodedURLInputs = this.form.children("input[name='javax.faces.encodedURL']");
+                // var originalAction = encodedURLInputs.length > 0 ? encodedURLInputs.val() : this.form.attr("action");
+                // var originalEncoding = this.form.attr("encoding");
+                // var originalEnctype = this.form.attr("enctype");
+                // try {
+                    // var delimiter = originalAction.indexOf("?") == -1 ? "?" : "&";
+                    // this.form.attr("action", originalAction + delimiter + UID + "=" + this.loadableItem.uid);
+                    // this.form.attr("encoding", "multipart/form-data");
+                    // this.form.attr("enctype", "multipart/form-data");
+                 //rf.submitForm(this.form, {"org.richfaces.ajax.component": this.id}, this.id);
+                 rf.Event.fire(this.element, "onfilesubmit", this.loadableItem.model);
+                // } finally {
+                    // this.form.attr("action", originalAction);
+                    // this.form.attr("encoding", originalEncoding);
+                    // this.form.attr("enctype", originalEnctype);
+                    // this.loadableItem.input.removeAttr("name");
+                // }
+             },
 
-            __load: function(event) {
-                if (this.loadableItem) {
-                    var contentDocument = event.target.contentWindow.document;
-                    contentDocument = contentDocument.XMLDocument || contentDocument;
-                    var documentElement = contentDocument.documentElement;
-                    var responseStatus, id;
-                    if (documentElement.tagName.toUpperCase() == "PARTIAL-RESPONSE") {
-                        var errors = $(documentElement).children("error");
-                        responseStatus = errors.length > 0 ? ITEM_STATE.SERVER_ERROR : ITEM_STATE.DONE;
-                    } else if ((id = documentElement.id) && id.indexOf(UID + this.loadableItem.uid + ":") == 0) {
-                        responseStatus = id.split(":")[1];
-                    }
-                    if (responseStatus) {
-                        var responseContext = {
-                            source: this.element[0],
-                            element: this.element[0],
-                            /* hack for MyFaces */
-                            _mfInternal: {
-                                _mfSourceControlId: this.element.attr('id')
-                            }
-                        };
+            // __load: function(event) {
+            //     console.log('__load() executed');
+            //     if (this.loadableItem) {
+            //         var contentDocument = event.target.contentWindow.document;
+            //         contentDocument = contentDocument.XMLDocument || contentDocument;
+            //         var documentElement = contentDocument.documentElement;
+            //         var responseStatus, id;
+            //         if (documentElement.tagName.toUpperCase() == "PARTIAL-RESPONSE") {
+            //             var errors = $(documentElement).children("error");
+            //             responseStatus = errors.length > 0 ? ITEM_STATE.SERVER_ERROR : ITEM_STATE.DONE;
+            //         } else if ((id = documentElement.id) && id.indexOf(UID + this.loadableItem.uid + ":") == 0) {
+            //             responseStatus = id.split(":")[1];
+            //         }
+            //         if (responseStatus) {
+            //             var responseContext = {
+            //                 source: this.element[0],
+            //                 element: this.element[0],
+            //                 /* hack for MyFaces */
+            //                 _mfInternal: {
+            //                     _mfSourceControlId: this.element.attr('id')
+            //                 }
+            //             };
 
-                        responseStatus == ITEM_STATE.DONE && jsf.ajax.response({responseXML: contentDocument}, responseContext);
-                        this.loadableItem.finishUploading(responseStatus);
-                        this.submitedItems.push(this.loadableItem);
-                        if (responseStatus == ITEM_STATE.DONE && this.items.length) {
-                            this.__startUpload();
-                        } else {
-                            this.loadableItem = null;
-                            this.__updateButtons();
-                            var items = [];
-                            for (var i in this.submitedItems) {
-                                items.push(this.submitedItems[i].model);
-                            }
-                            for (var i in this.items) {
-                                items.push(this.items[i].model);
-                            }
-                            rf.Event.fire(this.element, "onuploadcomplete", items);
-                        }
-                    }
-                }
-            },
+            //             responseStatus == ITEM_STATE.DONE && jsf.ajax.response({responseXML: contentDocument}, responseContext);
+            //             this.loadableItem.finishUploading(responseStatus);
+            //             this.submitedItems.push(this.loadableItem);
+            //             if (responseStatus == ITEM_STATE.DONE && this.items.length) {
+            //                 this.__startUpload();
+            //             } else {
+            //                 this.loadableItem = null;
+            //                 this.__updateButtons();
+            //                 var items = [];
+            //                 for (var i in this.submitedItems) {
+            //                     items.push(this.submitedItems[i].model);
+            //                 }
+            //                 for (var i in this.items) {
+            //                     items.push(this.items[i].model);
+            //                 }
+            //                 rf.Event.fire(this.element, "onuploadcomplete", items);
+            //             }
+            //         }
+            //     }
+            // },
 
             __accept: function(fileName) {
                 fileName = fileName.toUpperCase();
@@ -356,10 +391,10 @@
     })());
 
 
-    var Item = function(fileUpload, fileName) {
+    var Item = function(fileUpload, file) {
         this.fileUpload = fileUpload;
         this.input = fileUpload.input;
-        this.model = {name: fileName, state: ITEM_STATE.NEW};
+        this.model = {name: file.name, state: ITEM_STATE.NEW, file: file};
     };
 
     $.extend(Item.prototype, {
@@ -387,22 +422,108 @@
                 this.input.attr("name", this.fileUpload.id);
                 this.model.state = ITEM_STATE.UPLOADING;
                 this.uid = Math.random();
-                this.fileUpload.__submit();
-                if (this.fileUpload.progressBar) {
-                    this.fileUpload.progressBar.setValue(0);
-                    this.state.html(this.fileUpload.progressBarElement.detach());
 
-                    var params = {};
-                    params[UID_ALT] = this.uid;
-                    this.fileUpload.progressBar.enable(params);
+                var xhr = new XMLHttpRequest(),
+                    formData = new FormData(this.fileUpload.form[0]);
+                    fileName = this.model.file.name;
+
+                formData.append(this.fileUpload.id, this.model.file);
+
+                var originalAction = this.fileUpload.form.attr("action");
+                var delimiter = originalAction.indexOf("?") == -1 ? "?" : "&";
+                var newAction =  originalAction + delimiter + UID + "=" + this.uid;
+
+                xhr.onprogress = function(e) {
+                    console.log('progress');
+                    console.log(fileName);
+                    console.log(e || 'no event');
+                };
+
+                xhr.onload = $.proxy(function (e) {
+                    console.log('load');
+                    console.log(fileName);
+                    console.log(e || 'no event');
+                    this.fileUpload.__startUpload();
+                    }, this);
+
+                var state = ITEM_STATE.DONE;
+                this.input.remove();
+                this.state.html(this.fileUpload[state + "Label"]);
+                this.link.html(this.fileUpload["clearLabel"]);
+                this.model.state = state;
+
+                formData.append('javax.faces.partial.ajax', 'true');
+                formData.append('javax.faces.source', this.fileUpload.id);
+
+                var render = this.fileUpload.render,
+                    execute = this.fileUpload.execute,
+                    renderString, executeString;
+
+                if (render != '@none') {
+                    if (!render) {
+                        renderString = this.fileUpload.id;
+                    }
+                    else {
+                        switch(render) {
+                            case '@all':
+                                renderString = '@all';
+                            case '@form':
+                                renderString = this.fileUpload.form[0].id;
+                                break;
+                            case '@this':
+                                renderString = this.fileUpload.id;
+                                break;
+                            default:
+                                renderString = render;
+                        }
+                    }
+
+                    formData.append('javax.faces.partial.render', renderString);
                 }
+
+                if (execute != '@none') {
+                    if (!execute) {
+                        executeString = this.fileUpload.id;
+                    }
+                    else {
+                        switch(execute) {
+                            case '@all':
+                                executeString = '@all';
+                            case '@form':
+                                executeString = this.fileUpload.form[0].id;
+                                break;
+                            case '@this':
+                                executeString = this.fileUpload.id;
+                                break;
+                            default:
+                                executeString = execute;
+                        }
+                    }
+
+                    formData.append('javax.faces.partial.execute', executeString);
+                }
+
+                xhr.open('POST', newAction, true);
+
+                xhr.send(formData);
+                //this.fileUpload.__submit();
+                // if (this.fileUpload.progressBar) {
+                   // this.fileUpload.progressBar.setValue(0);
+                   // this.state.html(this.fileUpload.progressBarElement.detach());
+
+                   // var params = {};
+                   // params[UID_ALT] = this.uid;
+                  //  this.fileUpload.progressBar.enable(params);
+                //}
+
+                this.finishUploading();
             },
 
             finishUploading: function(state) {
-                if (this.fileUpload.progressBar) {
-                    this.fileUpload.progressBar.disable();
-                    this.fileUpload.hiddenContainer.append(this.fileUpload.progressBarElement.detach());
-                }
+                // if (this.fileUpload.progressBar) {
+                //     this.fileUpload.progressBar.disable();
+                //     this.fileUpload.hiddenContainer.append(this.fileUpload.progressBarElement.detach());
+                // }
                 this.input.remove();
                 this.state.html(this.fileUpload[state + "Label"]);
                 this.link.html(this.fileUpload["clearLabel"]);
